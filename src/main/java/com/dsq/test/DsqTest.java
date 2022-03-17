0ccfeb823bfd26123177b2a5e190ced8ff7dd8a9
duanshaoqian <duanshaoqian@51tuiyi.com>
2022-03-17 18:19:24 +0800

    2

---
+++ src/main/java/com/dsq/test/DsqTest.java
@@ -0,0 +1,15 @@
+package com.dsq.test;
+
+import com.dsq.tool.DsqTool;
+
+import java.util.ArrayList;
+
+public class DsqTest {
+
+    public static void main(String[] args) {
+        ArrayList<String> strings = DsqTool.readTxt("aaaa.txt");
+        for (String string : strings) {
+            System.out.println(string);
+        }
+    }
+}
