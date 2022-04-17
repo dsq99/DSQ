4f7faa179156e5cbf4e7655438e94534b48c2de0
duanshaoqian <duanshaoqian@51tuiyi.com>
2022-04-17 18:43:22 +0800

    4

--- src/main/java/com/dsq/study/javase/DataType.java
+++ src/main/java/com/dsq/study/javase/DataType.java
@@ -4,5 +4,25 @@ public class DataType {
     public static void main(String[] args) {
 
 
+        /**
+         *
+         * 1 bit表示1位
+
+         * 1 byte 表示一个字节 1B= 8b
+         *
+         * 基本数据类型
+         *      byte    1个字节
+         *      short   两个字节
+         *      int     4个字节
+         *      long    8个字节
+         *      float
+         *      double
+         *      char    两个字节
+         *
+         * 引用数据类型
+         */
+
+
+
     }
 }
