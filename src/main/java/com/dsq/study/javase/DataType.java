51633338192dd9d98bdf4406bb3bedc7219014ee
duanshaoqian <duanshaoqian@51tuiyi.com>
2022-07-21 14:04:11 +0800

    随便提交

--- src/main/java/com/dsq/study/javase/DataType.java
+++ src/main/java/com/dsq/study/javase/DataType.java
@@ -1,5 +1,7 @@
 package com.dsq.study.javase;
 
+import java.math.BigDecimal;
+
 public class DataType {
     public static void main(String[] args) {
 
@@ -11,17 +13,31 @@ public class DataType {
          * 1 byte 表示一个字节 1B= 8b
          *
          * 基本数据类型
-         *      byte    1个字节
-         *      short   两个字节
-         *      int     4个字节
+         *      byte    1个字节    -128-127
+         *      short   两个字节    正负 65535除以2 -32768 到 32767
+         *      int     4个字节    正负21亿
          *      long    8个字节
-         *      float
-         *      double
+         *      float    4个字节
+         *      double  8个字节
          *      char    两个字节
+         *      boolean 1位
          *
          * 引用数据类型
+         *      其他的都是引用
          */
 
+        // 二进制 0b
+        int i=10;
+        int i2=010;//八进制0
+        int i3=0x10;//ナ六进制 0x
+
+//        BigDecimal; 银行用这个类表示钱数字工具类
+
+        /**
+         * 所有的字符本质还是数字
+         * 编码Unicode表：(97=a65=A)2字节0-65536 ExceL 216=65536
+         * u0000 UFFFF
+         */
 
 
     }
